9d1e2c0d6f82390640b39cd3b87b050656a4099e
DasherPR <camiloandres1503@gmail.com>
2022-07-14 21:08:42 -0500

    added error if negative numbers are in input space

--- PesoPlaneta.js
+++ PesoPlaneta.js
@@ -11,13 +11,20 @@ function CalcularMarte(peso){
 function calcularPeso(){
     const inputPeso = document.getElementById("weight");
     const peso = inputPeso.value;
-    const pesoMarte = CalcularMarte(peso);
-    const pesoJupiter = CalcularJupiter(peso);
-    const roundedMarte = Math.round(pesoMarte * 10) / 10;
-    const roundedJupiter = Math.round(pesoJupiter * 10) / 10;
-    let resultMars = document.getElementById("mars");
-    let resultJupiter = document.getElementById("jupiter");
-    resultMars.innerText = roundedMarte + " Kg";
-    resultJupiter.innerText = roundedJupiter + " Kg";
+    if ( peso < 0 ){
+        var error = document.getElementById("error");
+        error.innerText = "los numeros deben ser positivos";
+    } else {
+        var error = document.getElementById("error");
+        error.innerText = ""
+        const pesoMarte = CalcularMarte(peso);
+        const pesoJupiter = CalcularJupiter(peso);
+        const roundedMarte = Math.round(pesoMarte * 10) / 10;
+        const roundedJupiter = Math.round(pesoJupiter * 10) / 10;
+        let resultMars = document.getElementById("mars");
+        let resultJupiter = document.getElementById("jupiter");
+        resultMars.innerText = roundedMarte + " Kg";
+        resultJupiter.innerText = roundedJupiter + " Kg";
+    }
 
 }
